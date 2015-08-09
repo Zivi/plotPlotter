46d0af20989f8fef5db715c665ce89847dc1c80e
Zivi Weinstock <zivi.weinstock@gmail.com>
2015-08-09 16:00:13 -0700

    Bootstrap, map div added.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+function initialize() {
+  var mapOptions = {
+    center: { lat: 37.5542, lng: -122.3131},
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  console.log("map loaded")
+}
+google.maps.event.addDomListener(window, 'load', initialize);
