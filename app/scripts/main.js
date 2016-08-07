6bd6a295e169ee02209f1c9a27dce2680ee5563e
Zivi Weinstock <zivi.weinstock@gmail.com>
2016-08-07 16:29:14 -0700

    CSS styling.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,25 +7,33 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  console.log("map loaded");
+  console.log('map loaded');
 
   $('.plot-names').click(function(event) {
     var marker,
-    var $this = $(event.currentTarget),
-    var nom = $this.attr('name'),
-    var lat = $this.data('lat'),
-    var lng = $this.data('lng'),
+      infowindow,
+      $this = $(event.currentTarget),
+      nom = $this.attr('name'),
+      lat = $this.data('lat'),
+      lng = $this.data('lng');
     if ($this.prop('checked')) {
       marker = new google.maps.Marker({
         position: {lat: lat, lng: lng},
         map: map,
         title: nom
       });
+      infowindow = new google.maps.InfoWindow({
+        content: nom
+      });
+      infowindow.open(map, marker);
       $this.data('marker', marker);
     } else {
       marker = $this.data('marker');
       marker.setMap(null);
     }
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
   });
 }
 
